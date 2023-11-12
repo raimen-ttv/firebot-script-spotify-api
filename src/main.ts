d46c03351ba954120f1da8a19d2ef76a9f9380f5
Raimen <84481540+raimen-ttv@users.noreply.github.com>
2023-11-12 17:17:17 -0500

    non-working unable to build:dev

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import {
   RunRequest,
   ScriptReturnObject
 } from "@crowbartools/firebot-custom-scripts-types";
+
+import { initLogger } from "./logger";
 import { EffectTriggerResponse, Effects } from "@crowbartools/firebot-custom-scripts-types/types/effects";
 import { Control } from 'magic-home';
 import { Discovery } from 'magic-home';
 import { Logger } from "@crowbartools/firebot-custom-scripts-types/types/modules/logger";
-import { LocalStorage } from "node-localstorage";
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
-const localStorage = new LocalStorage("./magicHome")
+import {SpotifyApi, AuthorizationCodeWithPKCEStrategy} from '@spotify/web-api-ts-sdk';
 
 type Params = {
   credential: string 
@@ -20,10 +19,10 @@ type Params = {
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
     return {
-      name: "Hype guy controller",
-      description: "for magic home",
+      name: "Spotify for Firebot",
+      description: "Connect to Spotify API directly from Firebot!",
       author: "Raimen",
-      version: "1.5",
+      version: "1.0",
       firebotVersion: "5",
     };
   },
@@ -38,24 +37,21 @@ const script: Firebot.CustomScript<Params> = {
     };
   },
 
-  run: async (): Promise<ScriptReturnObject> => {
+  run: async ({parameters, modules}) => {
+    const { logger, effectManager, replaceVariableManager } = modules;
+    initLogger(logger);
 
-    // credentials are optional
-    var spotifyApi = new SpotifyWebApi({
-      clientId: '74f8735962614cf8b5ba72709d2eac5f',
-      clientSecret: '2bcb657eda324ec18ffa03f37d8cb95f',
-      redirectUri: 'localhost:6969'
-    });
+    const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
+      "74f8735962614cf8b5ba72709d2eac5f",
+      "localhost:3000",
+      ['playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state']
+    );
+    const spotify = new SpotifyApi(implicitGrantStrategy)
 
-    let thing: ScriptReturnObject = {
-      success: true,
-      // success: false,
-      errorMessage: "Failed to run the script!", // If 'success' is false, this message is shown in a Firebot popup.
-      effects: []
-    }
-    return thing
+    const profile = await spotify.currentUser.profile();
+    logger.debug(JSON.stringify(profile));
+
   },
 };
-
 
 export default script;
