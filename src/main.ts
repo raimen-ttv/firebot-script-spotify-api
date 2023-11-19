a03341cbcd18aed637aefc97d2a6b0ceab1c2e78
Raimen <84481540+raimen-ttv@users.noreply.github.com>
2023-11-19 14:59:32 -0500

    Spotify building but not importing

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ import {
 
 import { initLogger } from "./logger";
 import { EffectTriggerResponse, Effects } from "@crowbartools/firebot-custom-scripts-types/types/effects";
-import { Control } from 'magic-home';
-import { Discovery } from 'magic-home';
 import { Logger } from "@crowbartools/firebot-custom-scripts-types/types/modules/logger";
+import SpotifyWebApi from 'spotify-web-api-node'
 
-import {SpotifyApi, AuthorizationCodeWithPKCEStrategy} from '@spotify/web-api-ts-sdk';
+// import {SpotifyApi, AuthorizationCodeWithPKCEStrategy} from '@spotify/web-api-ts-sdk';
 
 type Params = {
   credential: string 
@@ -41,15 +40,41 @@ const script: Firebot.CustomScript<Params> = {
     const { logger, effectManager, replaceVariableManager } = modules;
     initLogger(logger);
 
-    const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
-      "74f8735962614cf8b5ba72709d2eac5f",
-      "localhost:3000",
-      ['playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state']
-    );
-    const spotify = new SpotifyApi(implicitGrantStrategy)
 
-    const profile = await spotify.currentUser.profile();
-    logger.debug(JSON.stringify(profile));
+
+    var scopes = ['playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state'],
+    redirectUri = 'http://localhost:3000',
+    clientId = '74f8735962614cf8b5ba72709d2eac5f',
+    state = 'login',
+    showDialog = true,
+    responseType = 'token';
+
+    // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+    var spotifyApi = new SpotifyWebApi({
+      redirectUri: redirectUri,
+      clientId: clientId
+    });
+
+    // Create the authorization URL
+    var authorizeURL = spotifyApi.createAuthorizeURL(
+      scopes,
+      state,
+      showDialog
+    );
+
+    // https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=token&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice&show_dialog=true
+    logger.info(authorizeURL);
+
+
+    // const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
+    //   "74f8735962614cf8b5ba72709d2eac5f",
+    //   "localhost:3000",
+    //   ['playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state']
+    // );
+    // const spotify = new SpotifyApi(implicitGrantStrategy)
+
+    // const profile = await spotify.currentUser.profile();
+    // logger.debug(JSON.stringify(profile));
 
   },
 };
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -8,19 +8,19 @@ test("main default export is the custom script", () => {
   expect(customScript.getDefaultParameters).not.toBeUndefined();
 });
 
-test("run() calls logger.info with the message", async () => {
-  const mockInfoLog = jest.fn<
-    void,
-    ArgumentsOf<ScriptModules["logger"]["info"]>
-  >();
-  const expectedMessage = "foobar";
-  const runRequest = ({
-    parameters: { message: expectedMessage },
-    modules: { logger: { info: mockInfoLog } },
-  } as unknown) as RunRequest<any>;
+// test("run() calls logger.info with the message", async () => {
+//   const mockInfoLog = jest.fn<
+//     void,
+//     ArgumentsOf<ScriptModules["logger"]["info"]>
+//   >();
+//   const expectedMessage = "foobar";
+//   const runRequest = ({
+//     parameters: { message: expectedMessage },
+//     modules: { logger: { info: mockInfoLog } },
+//   } as unknown) as RunRequest<any>;
 
-  await customScript.run(runRequest);
+//   await customScript.run(runRequest);
 
-  expect(mockInfoLog.mock.calls.length).toBe(1);
-  expect(mockInfoLog.mock.calls[0][0]).toBe(expectedMessage);
-});
+//   expect(mockInfoLog.mock.calls.length).toBe(1);
+//   expect(mockInfoLog.mock.calls[0][0]).toBe(expectedMessage);
+// });
