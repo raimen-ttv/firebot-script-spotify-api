9c99de68f81541c6b8b6c954e75131756b21c4e6
raimen-ttv <raimen.gg@gmail.com>
2024-04-24 01:36:38 -0400

    eod changes

---
+++ src/integration-definition.ts
@@ -0,0 +1,172 @@
+"use strict";
+import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
+import EventEmitter from "events";
+
+import { initLogger, logger } from "./logger";
+import { FirebotParameterCategories, FirebotParams } from "@crowbartools/firebot-custom-scripts-types/types/modules/firebot-parameters";
+import myTypes from "@crowbartools/firebot-custom-scripts-types";
+import { IntegrationDefinition } from "@crowbartools/firebot-custom-scripts-types/types/modules/integration-manager";
+
+export type myIntegration<Params extends IntegrationDefinition = IntegrationDefinition> = {
+  
+}
+
+
+export type OldIntegrationDefinition<Params extends FirebotParams = FirebotParams> = {
+    id: string;
+    name: string;
+    description: string;
+    connectionToggle?: boolean;
+    configurable?: boolean;
+    settingCategories: FirebotParameterCategories<Params>;
+  } & (
+    | {
+      linkType: "id";
+      idDetails: {
+        steps: string;
+      };
+    }
+    | {
+      linkType: "auth";
+      authProviderDetails: {
+        id: string;
+        name: string;
+        redirectUriHost?: string;
+        client: {
+          id: string;
+          secret: string;
+        };
+        auth: {
+          type?: string
+          tokenHost: string;
+          tokenPath: string;
+          authorizePath: string;
+          authorizeHost?: string;
+        };
+        autoRefreshToken?: boolean;
+        scopes: string;
+      };
+    }
+    | { linkType: "other" | "none" }
+  );
+
+interface client {
+  id: string;
+  secret: string;
+};
+
+interface SpotifyUser {
+  display_name: string;
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: string | null;
+    total: number;
+  };
+  href: string;
+  id: string;
+  images: {
+    url: string;
+    height: number;
+    width: number;
+  }[];
+  type: string;
+  uri: string;
+}
+
+export let secret: client = {
+  id: "74f8735962614cf8b5ba72709d2eac5f",
+  secret: "",
+}
+
+export function genDef(): IntegrationDefinition {
+  return {
+    id: "Spotify",
+    name: "Spotify",
+    description: "Get current song information, control playback, add to queue, and access playlist information.",
+    connectionToggle: true,
+    linkType: "auth",
+    settingCategories: {
+    },
+    authProviderDetails: {
+      id: "Spotify",
+      name: "Spotify",
+      redirectUriHost: "localhost",
+      client: secret,
+      auth: {
+        type: 'token',
+        tokenHost: "https://accounts.spotify.com",
+        tokenPath: '/api/token',
+        authorizePath: '/authorize?'
+      },
+      autoRefreshToken: true,
+      scopes: `user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read`
+    }
+  }
+}
+
+class SpotifyIntegration extends EventEmitter {
+
+  connected: boolean;
+  definition: IntegrationDefinition;
+  private accessToken: string | null;
+  private userData: SpotifyUser;
+  private headers: { headers: { Authorization: string; }; };
+
+  constructor() {
+    super();
+    this.connected = false;
+    this.definition = genDef()
+  }
+  init() {
+
+  }
+  connect(integrationData: { settings: any; }) {
+    const { settings } = integrationData;
+
+    if (settings == null) {
+      this.emit("disconnected", this.definition.id);
+      return;
+    }
+
+    this.emit("connected", this.definition.id);
+    this.connected = true;
+  }
+  disconnect() {
+    this.connected = false;
+
+    this.emit("disconnected", this.definition.id);
+  }
+  async link(linkData: { auth: any; }) {
+    try {
+      const { auth } = linkData;
+      logger.info(`Linking ${this.definition.id}`);
+      this.accessToken = auth['accessToken'];
+      this.headers = { headers: { Authorization: `Bearer  + ${this.accessToken}` } };
+
+      const response = await fetch('https://api.spotify.com/v1/me', this.headers);
+      const data = await response.json();
+
+      logger.info(`Linked successfully to ${this.definition.id} as ${this.userData.display_name}`);
+    } catch (e) {
+      logger.error(`Error linking ${this.definition.id}: ${e}`);
+    }
+  }
+  unlink() {
+    this.emit("unlinked", this.definition.id);
+  }
+
+  async getProfile() {
+    const response = await fetch('https://api.spotify.com/v1/me', this.headers);
+    this.userData = await response.json();
+  }
+
+  async getCurrentlyPlaying() {
+    const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', this.headers);
+    let data = await response.json();
+    return data;
+  }
+}
+
+export const integration = new SpotifyIntegration();
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,9 @@ import {
 import { initLogger } from "./logger";
 import { EffectTriggerResponse, Effects } from "@crowbartools/firebot-custom-scripts-types/types/effects";
 import { Logger } from "@crowbartools/firebot-custom-scripts-types/types/modules/logger";
-import SpotifyWebApi = require("spotify-web-api-node")
-import * as express from "express";
-import { Request, Response } from "express";
-import { initSpotify } from "./spotifyHelper";
+// import * as express from "express";
+// import { Request, Response } from "express";
+import { genDef, integration } from "./integration-definition";
 import { spotifyRequestEffect } from "./firebot/effects/send-api-request";
 // import * as path from "path";
 // import open from "open";
@@ -25,7 +24,7 @@ const script: Firebot.CustomScript<Params> = {
       name: "Spotify for Firebot",
       description: "Connect to Spotify API directly from Firebot!",
       author: "Raimen",
-      version: "1.0",
+      version: "0.1",
       firebotVersion: "5",
       website: "https://twitch.tv/raimen"
     };
@@ -35,61 +34,22 @@ const script: Firebot.CustomScript<Params> = {
     return {
       credential: {
         type: "string",
-        default: "asdf",
+        default: "",
         description: "Credential key",
-        secondaryDescription: "You must authenticate with Spotify to get this key. Open this url to get started: https://bit.ly/3Rjaz2d"
+        secondaryDescription: "If you do not want to use the default spotify app, you can get your own from Spotify Developer Dashboard."
       }
     };
   },
 
   run: async ({parameters, modules}) => {
     const { logger, effectManager, replaceVariableManager } = modules;
+
     initLogger(logger);
-    await initSpotify();
-    logger.info("post spotify init");
+    modules.integrationManager.registerIntegration({integration, definition:integration.definition});
+    modules.integrationManager.integrationIsConnectable("Spotify");
+    let response = modules.integrationManager.getIntegrationById("Spotify");
+    logger.debug(JSON.stringify(response));
 
-    const app = express();
-    const port = 3000;
-
-    // app.get('/', (req, res) => {
-    //   logger.info("got request: " + JSON.stringify(req.query));
-    //   const customContent = String(req.query.code)
-    //   res.render('index', { customContent });
-    // })
-    
-    
-    // Endpoint for Spotify redirect
-    app.get('/', (req: Request, res: Response) => {
-        const { code, error } = req.query;
-    
-        // Handle the query parameters as needed
-        if (typeof error === 'string') {
-            // Handle error scenario (e.g., show error message to the user)
-            res.send(`Authentication failed: ${error}`);
-        } else if (typeof code === 'string') {
-            res.send(`You have successfully authenticated with Spotify! Your code: ${code}`);
-        } else {
-            // Handle unexpected case
-            res.send('Invalid response from Spotify');
-        }
-    });
-    
-    app.listen(port, () => {
-      logger.info(`Spotify integration listening at http://localhost:${port}`);
-    });
-
-    // open(authorizeURL);
-
-
-    // const implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
-    //   "74f8735962614cf8b5ba72709d2eac5f",
-    //   "localhost:3000",
-    //   ['playlist-modify-public', 'playlist-modify-private, user-read-playback-state, user-modify-playback-state']
-    // );
-    // const spotify = new SpotifyApi(implicitGrantStrategy)
-
-    // const profile = await spotify.currentUser.profile();
-    // logger.debug(JSON.stringify(profile));
     effectManager.registerEffect(spotifyRequestEffect)
   },
 };
